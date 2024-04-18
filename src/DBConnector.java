0399dfeed546c0abe09283f694c061bc341cecea
yuya-6 <yuchongakufu@gmail.com>
2024-04-18 21:11:02 +0900

    5th commit

---
+++ src/DBConnector.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+	private static String driverName = "com.mysql.jdbc.Driver";
+
+	private static String url =
+			"jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
+
+	private static String user = "root";
+	private static String password = "0114Yuya";
+
+	public Connection getConnection() {
+		Connection con = null;
+
+		try{
+			Class.forName(driverName);
+			con = DriverManager.getConnection(url,user,password);
+			}
+
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+			}
+
+		return con ;
+
+	}
+
+}
+
+
